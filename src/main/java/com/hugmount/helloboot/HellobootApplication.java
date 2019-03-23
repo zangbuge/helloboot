75a77fbe430e8937d266e09c800595a70814df06
zangbuge <1469987440@qq.com>
2019-03-23 11:05:09 +0800

    yml工具类

--- src/main/java/com/hugmount/helloboot/HellobootApplication.java
+++ src/main/java/com/hugmount/helloboot/HellobootApplication.java
@@ -3,8 +3,10 @@ package com.hugmount.helloboot;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+// exclude = 将排除不加载这些类, 避免默认加载这些未使用的类而导致报错的问题
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class , HibernateJpaAutoConfiguration.class})
 @MapperScan("com.hugmount.helloboot.*.mapper")
 public class HellobootApplication {
 
