315e9b9e85d59ddefab61b8bc141b8386b2d1a70
zangbuge <1469987440@qq.com>
2019-06-01 16:22:00 +0800

    Callable

--- src/test/java/com/hugmount/helloboot/TestThread.java
+++ src/test/java/com/hugmount/helloboot/TestThread.java
@@ -1,6 +1,12 @@
 package com.hugmount.helloboot;
 
+import com.alibaba.fastjson.JSON;
 import com.hugmount.helloboot.util.ThreadUtil;
+import org.junit.Test;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
 /**
  * @Author: Li Huiming
@@ -19,4 +25,42 @@ public class TestThread {
             });
         }
     }
+
+
+    @Test
+    public void TestCallable() {
+        ExecutorService executorService = ThreadUtil.getExecutorService();
+        CallableDemo callableDemo = new CallableDemo("lhm");
+        Future submit = executorService.submit(callableDemo);
+        System.out.println(JSON.toJSONString(submit));
+        executorService.shutdown();
+        System.out.println(JSON.toJSONString(submit));
+    }
+
+    public class CallableDemo implements Callable {
+
+        private String name;
+
+        public CallableDemo(String name){
+            this.name = name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        /**
+         * Computes a result, or throws an exception if unable to do so.
+         * @return computed result
+         *
+         * @throws Exception if unable to compute a result
+         */
+        @Override
+        public Object call() throws Exception {
+            System.out.println("使用callable开启的线程, name: " + name);
+            return null;
+        }
+    }
+
+
 }
