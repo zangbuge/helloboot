eee65bce941800f8b4b0f35fb6639c8b6d36f2de
hugmount <331549227@qq.com>
2019-08-18 13:29:11 +0800

    vo

---
+++ src/main/java/com/hugmount/helloboot/test/pojo/User.java
@@ -0,0 +1,17 @@
+package com.hugmount.helloboot.test.pojo;
+
+import lombok.Data;
+
+/**
+ * @Author Li Huiming
+ * @Date 2019/8/17
+ */
+
+@Data
+public class User {
+
+    private  String username;
+
+    private String password;
+
+}
