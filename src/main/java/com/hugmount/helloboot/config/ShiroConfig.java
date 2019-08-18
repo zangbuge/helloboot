9e4a14635212a228d6ee7b72f349cb98e20b3047
hugmount <331549227@qq.com>
2019-08-18 13:24:53 +0800

    整合shiro

---
+++ src/main/java/com/hugmount/helloboot/config/ShiroConfig.java
@@ -0,0 +1,148 @@
+package com.hugmount.helloboot.config;
+
+import com.hugmount.helloboot.test.pojo.User;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.realm.Realm;
+import org.apache.shiro.spring.web.config.DefaultShiroFilterChainDefinition;
+import org.apache.shiro.spring.web.config.ShiroFilterChainDefinition;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author Li Huiming
+ * @Date 2019/8/17
+ */
+
+@Configuration
+public class ShiroConfig {
+
+    @Bean
+    public Realm realm() {
+        //创建自己的Realm实例
+        return new UserRealm();
+    }
+
+    @Bean
+    public DefaultWebSessionManager getDefaultWebSessionManager() {
+        DefaultWebSessionManager defaultWebSessionManager = new DefaultWebSessionManager();
+        defaultWebSessionManager.setGlobalSessionTimeout(1000 * 60 * 30);// 会话过期时间,单位：毫秒(在无操作时开始计时)
+        defaultWebSessionManager.setSessionValidationSchedulerEnabled(true);
+        defaultWebSessionManager.setSessionIdCookieEnabled(true);
+        return defaultWebSessionManager;
+    }
+
+    @Bean(name = "securityManager")
+    public DefaultWebSecurityManager getDefaultWebSecurityManager() {
+        DefaultWebSecurityManager dwsm = new DefaultWebSecurityManager();
+        dwsm.setRealm(realm());// 自定义realm
+        dwsm.setSessionManager(getDefaultWebSessionManager());// session管理
+        return dwsm;
+    }
+
+
+    /**
+     * 设置对应的过滤条件和跳转条件
+     * @return
+     */
+    @Bean
+    public ShiroFilterChainDefinition shiroFilterChainDefinition() {
+        DefaultShiroFilterChainDefinition chain = new DefaultShiroFilterChainDefinition();
+
+        // 访问控制 ,注意过滤器配置顺序 不能颠倒
+        chain.addPathDefinition("/download_error", "anon");// 可以匿名访问
+        chain.addPathDefinition("/404", "anon");//404页面
+        chain.addPathDefinition("/login", "anon");//登录不能拦截
+        chain.addPathDefinition("/css/**", "anon");//静态资源文件
+        chain.addPathDefinition("/js/**", "anon");
+        // 其它路径均需要登录
+        chain.addPathDefinition("/**", "authc");//其他使用注解判断
+
+        return chain;
+    }
+
+
+    /**
+     * 验证以及权限添加（自定义权限添加及检查）
+     */
+    static class UserRealm extends AuthorizingRealm {
+
+        /**
+         *  添加权限
+         * @param principalCollection
+         * @return
+         */
+        @Override
+        protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+            //获取登录用户名
+            String name= (String) principalCollection.getPrimaryPrincipal();
+            //查询用户名称（这里可以使用缓存来做），这里根据项目情况不同，选择不同的操作
+//            User user = loginService.findByName(name);
+            String user = "hugmount";
+
+            //添加角色和权限
+            SimpleAuthorizationInfo simpleAuthorizationInfo = new SimpleAuthorizationInfo();
+//            for (Role role:user.getRoles()) {
+//                //添加角色
+//                simpleAuthorizationInfo.addRole(role.getRoleName());
+//                for (Permission permission:role.getPermissions()) {
+//                    //添加权限
+//                    simpleAuthorizationInfo.addStringPermission(permission.getPermission());
+//                }
+//            }
+
+            simpleAuthorizationInfo.addStringPermission("all");
+            return simpleAuthorizationInfo;
+        }
+
+
+        /**
+         * 验证权限, 调用currUser.login(token)方法时会调用doGetAuthenticationInfo方法
+         * @param authenticationToken
+         * @return
+         * @throws AuthenticationException
+         */
+        @Override
+        protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+            //加这一步的目的是在Post请求的时候会先进认证，然后在到请求
+            if (authenticationToken.getPrincipal() == null) {
+                return null;
+            }
+            //获取shiro中用户信息
+            String name = authenticationToken.getPrincipal().toString();
+            System.out.println("shiro用户信息: " + name);
+            // 从数据库获取用户信息
+            User user = new User();
+            user.setUsername("hugmount");
+            user.setPassword("password");
+            if (user == null) {
+                //这里返回后会报出对应异常
+                return null;
+            }
+            else {
+                //这里验证authenticationToken和simpleAuthenticationInfo的信息, 匹配成功才验证通过
+                SimpleAuthenticationInfo simpleAuthenticationInfo = new SimpleAuthenticationInfo(user.getUsername(), user.getPassword(), getName());
+                return simpleAuthenticationInfo;
+            }
+        }
+
+    }
+
+/**
+ 用户登录成功后，通过
+ SecurityUtils.getSubject().getSession().setAttribute("user",userTo);
+
+ 设置shiro的session，开启一个线程加载列表，在列表中获取session
+ SecurityUtils.getSubject().getSession().getAttribute("user");
+ */
+
+
+}
---
+++ src/main/java/com/hugmount/helloboot/test/controller/UserController.java
@@ -0,0 +1,65 @@
+package com.hugmount.helloboot.test.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.hugmount.helloboot.test.pojo.User;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.subject.Subject;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author Li Huiming
+ * @Date 2019/8/17
+ */
+
+@RestController
+public class UserController {
+
+
+    @PostMapping("/login")
+    public String login(@RequestBody User user) {
+        System.out.println(JSON.toJSONString(user));
+        UsernamePasswordToken token = new UsernamePasswordToken(user.getUsername(), user.getPassword());
+        Subject currentUser = SecurityUtils.getSubject();
+        try {
+            // 登录
+            currentUser.login(token);
+            return JSON.toJSONString(user);
+
+        } catch (UnknownAccountException uae) {
+            return "账号不正确!";
+
+        } catch (IncorrectCredentialsException ice) {
+            return "密码不正确!";
+
+        } catch (LockedAccountException lae) {
+            return "账号被锁定!";
+
+        } catch (AuthenticationException ae) {
+            return "登录出错!";
+        }
+
+    }
+
+    @GetMapping("/logout")
+    public String logOut() {
+        Subject subject = SecurityUtils.getSubject();
+        subject.logout();
+        System.out.println("已退出");
+        return "logout--->已退出";
+    }
+
+
+//    @RequiresRoles("xxxx") 这里配置的是哪些角色可以使用
+//    @RequiresPermissions("xxxxxxx")这里配置的是哪些权限可以使用
+    @GetMapping("/testHello")
+    public String testHello() {
+        System.out.println("测试权限");
+
+        return "testHello";
+    }
+
+}
