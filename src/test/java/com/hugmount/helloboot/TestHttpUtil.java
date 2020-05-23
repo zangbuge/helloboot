236f519fa9d057634c185fff8e82d86263b8394b
lihuiming <1469987440@qq.com>
2020-05-23 13:34:58 +0800

    test Http

--- src/test/java/com/hugmount/helloboot/TestHttpUtil.java
+++ src/test/java/com/hugmount/helloboot/TestHttpUtil.java
@@ -1,11 +1,9 @@
 package com.hugmount.helloboot;
 
 import com.alibaba.fastjson.JSON;
-import com.hugmount.helloboot.util.CommonsHTTPUtil;
 import com.hugmount.helloboot.util.HttpClientUtil;
 import org.apache.http.impl.client.CloseableHttpClient;
 
-import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,28 +17,22 @@ public class TestHttpUtil {
 
     public static void main(String[] args) {
 
-        String urltest = "http://localhost:8080/test";
-        String urlhello = "http://localhost:8080/hello";
+        String testJson = "http://127.0.0.1:8001/testJson";
+        String testPost = "http://127.0.0.1:8001/testPost";
         Map<String, Object> header = new HashMap<>();
-        header.put("accept", "text/plain;charset=utf-8");
-        header.put("connection", "Keep-Alive");
-        header.put("Content-Type", "application/json");
-        header.put("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
-
+        header.put("token","1234567890");
         Map<String, Object> map = new HashMap<>();
-        map.put("productName","hello");
-        map.put("password","password");
-        map.put("username","李会明");
-        String urlLogin = "http://localhost:8086/helloboot/login";
+        map.put("userId","123");
+        map.put("nickname","李会明");
         CloseableHttpClient client = HttpClientUtil.getClient();
-//        String s1 = HttpClientUtil.doPost(urltest, map, null);
-        File file = new File("d:/正面.jpg");
-//        String s1 = HttpClientUtil.uploadFile(urltest, file ,"pic" ,map, null);
-        String s1 = HttpClientUtil.doPostJson(urlhello, JSON.toJSONString(map),null);
-//        String s1 = CommonsHTTPUtil.doPost(urltest, map, null);
-        System.out.println("登录返回" + s1);
+        String json = HttpClientUtil.doPostJson(testJson, JSON.toJSONString(map), null);
+        System.out.println("testJson返回: " + json);
 
+        String post = HttpClientUtil.doPost(testPost, map,null);
+        System.out.println("testPost返回" + post);
 
+        String s = HttpClientUtil.sendRequestKeepAlive(null, testPost, map, "post", header);
+        System.out.println(s);
 
         String url = "http://localhost:8086/helloboot/product/getProductList";
         header.put("accept", "*/*");
