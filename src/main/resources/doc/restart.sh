b199d131a8e19ada6958dffee5dbe4fb5b931da9
lihuiming <1469987440@qq.com>
2020-09-04 17:25:56 +0800

    整理脚本

--- src/main/resources/doc/restart.sh
+++ src/main/resources/doc/restart.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 pid=`ps -ef|grep helloboot |grep java|awk '{print $2}'`
-echo "old pid: $pid"
+echo "app old pid: $pid"
 if [ -n "$pid" ]
 then
 kill -9 $pid
 fi
 mvn clean package
 java -jar ./target/helloboot-0.0.1-SNAPSHOT.jar &
+echo "app start success"
 
-# 必须设置脚本格式为unix
-# vi deploy.sh   Windows中默认格式是dos. 查看命令  :set ff   修改为 :set ff=unix
-# jenkins 中配置 Execute shell命令:
 
+# 必须设置脚本格式为unix Windows中创建文本默认格式是dos
+# vi deploy.sh 查看命令 :set ff 修改为 :set ff=unix
+
+### 以下脚本配置在jenkins 构建脚本 Execute shell 命令:
 #当jenkins进程结束后新开的tomcat进程不被杀死
-BUILD_ID=DONTKILLME
-
+#BUILD_ID=DONTKILLME
 #加载环境变量
-. /etc/profile
-
+#. /etc/profile
 #sh /var/lib/jenkins/workspace/helloboot/restart.sh
-sh /var/lib/jenkins/workspace/deploy.sh
+#sh /var/lib/jenkins/workspace/deploy.sh
