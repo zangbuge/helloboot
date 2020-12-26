0231b42884aad71376c1bae043ea8d5abf330cac
lihuiming <1469987440@qq.com>
2020-12-26 17:58:24 +0800

    sh赋值问题

--- src/main/resources/sh/docker.sh
+++ src/main/resources/sh/docker.sh
@@ -1,6 +1,8 @@
-image_name=helloboot
-container_id = $(docker ps |grep "$image_name" |awk '{print $1}')
-image_id=$(docker images |grep "$image_name" |grep 0.0.1 | awk'{print $3}')
+image_name=helloboot  # 注意 `` 是反引号
+container_id=`docker ps |grep "$image_name" |awk '{print $1}'`
+image_id=`docker images |grep "$image_name" |grep 0.0.1 |awk '{print $3}'`
+echo "$container_id 容器ID"
+echo "$image_id 镜像ID"
 
 cd /var/jenkins_mount/workspace/helloboot
 docker stop $container_id
