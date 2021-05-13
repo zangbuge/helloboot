40dc9f78543b5f5c0fd88363d30c885bec76cc81
zangbuge <1469987440@qq.com>
2021-05-13 21:40:36 +0800

    导入

--- src/main/java/com/hugmount/helloboot/util/POIUtil.java
+++ src/main/java/com/hugmount/helloboot/util/POIUtil.java
@@ -5,10 +5,15 @@ import org.apache.poi.xssf.streaming.SXSSFCell;
 import org.apache.poi.xssf.streaming.SXSSFRow;
 import org.apache.poi.xssf.streaming.SXSSFSheet;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStream;
+import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 /**
@@ -18,6 +23,9 @@ import java.util.*;
  * @Date: 2021/5/12
  */
 public class POIUtil {
+
+    //日期格式
+    private static String formatStr = "yyyy-MM-dd HH:mm:ss";
 
     public static SXSSFWorkbook exportExcel(LinkedHashMap<String, String> headMap, List<Map<String, Object>> dataList) {
         // 声明一个工作薄
@@ -91,6 +99,7 @@ public class POIUtil {
         return workbook;
     }
 
+
     public static void downloadExcel(SXSSFWorkbook workbook, HttpServletResponse response, String fileName) {
         try {
             response.setContentType("application/msexcel;charset=utf-8");
@@ -101,8 +110,95 @@ public class POIUtil {
             // 释放workbook所占用的所有windows资源
             workbook.dispose();
         } catch (IOException e) {
-            throw new RuntimeException("download excel exception");
+            throw new RuntimeException("download excel exception", e);
         }
+    }
+
+
+    public static List<Map<String, Object>> importExcel(InputStream inputStream) {
+        try {
+            XSSFWorkbook wb = new XSSFWorkbook(inputStream);
+            SXSSFWorkbook sxssfWorkbook = new SXSSFWorkbook(wb);
+            SXSSFSheet sheetAt = sxssfWorkbook.getSheetAt(0);
+            if (sheetAt == null) {
+                throw new RuntimeException("该文件中没有excel数据");
+            }
+            List<Map<String, Object>> rowList = new ArrayList<>();
+            int lastRowNum = sheetAt.getLastRowNum();
+            for (int i = 0; i <= lastRowNum; i++) {
+                SXSSFRow row = sheetAt.getRow(i);
+                if (row == null) {
+                    continue;
+                }
+
+                // 创建保存一行数据map
+                Map<String, Object> rowData = new LinkedHashMap<>();
+                boolean isSkip = true;
+                short lastCellNum = row.getLastCellNum();
+                for (short j = 0; j <= lastCellNum; j++) {
+                    SXSSFCell cell = row.getCell(j);
+                    String cellValueStr = getCellValueStr(cell);
+                    rowData.put(String.valueOf(j), cellValueStr);
+                    if (!"".equals(cellValueStr)) {
+                        isSkip = false;
+                    }
+                }
+
+                if (isSkip) {
+                    continue;
+                }
+                rowList.add(rowData);
+            }
+            return rowList;
+        } catch (Exception e) {
+            throw new RuntimeException("解析excel失败", e);
+        }
+    }
+
+    static String getCellValueStr(SXSSFCell cell) {
+        String cellStr = "";
+        if (null == cell) {
+            return cellStr;
+        }
+        switch (cell.getCellTypeEnum()) {
+            case BOOLEAN: // 布尔值
+                cellStr = String.valueOf(cell.getBooleanCellValue());
+                break;
+            case FORMULA: // 读取公式
+                cellStr = cell.getCellFormula();
+                break;
+            // 日期或数字
+            case NUMERIC:
+                // 读取日期
+                if (DateUtil.isCellDateFormatted(cell)) {
+                    Date dateCellValue = cell.getDateCellValue();
+                    DateFormat dateFormat = new SimpleDateFormat(formatStr);
+                    cellStr = dateFormat.format(dateCellValue);
+                }
+                // 读取数字
+                else {
+                    double number = cell.getNumericCellValue();
+                    // 整数
+                    if (number == (long) number) {
+                        cellStr = String.valueOf((long) number);
+                    }
+                    // 包含小数的double
+                    else {
+                        DecimalFormat df = new DecimalFormat("0.00");
+                        // 设置diuble类型不转为"科学计数法"
+                        cellStr = df.format(cell.getNumericCellValue());
+                        // 此方式会将过度过长的数字转为"科学计数法" 显示
+//						cellStr = String.valueOf(cell.getNumericCellValue());
+                    }
+                }
+                break;
+
+            // 默认转换成String
+            default:
+                cellStr = cell.getStringCellValue();
+        }
+
+        return cellStr;
     }
 
 }
