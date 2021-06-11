0f24a07d4e25aacdc5e7486b3b5aaaf61d2de203
zangbuge <1469987440@qq.com>
2021-06-11 21:48:53 +0800

    redisson编码

--- src/main/java/com/hugmount/helloboot/config/RedissonConfig.java
+++ src/main/java/com/hugmount/helloboot/config/RedissonConfig.java
@@ -12,6 +12,7 @@ public class RedissonConfig {
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
+        config.setCodec(new org.redisson.client.codec.StringCodec());
 //        config.useSentinelServers() // 哨兵配置
 //        .setMasterName("my-sentinel-name") // 设置sentinel.conf配置里的sentinel别名
 //         这里设置sentinel节点的服务IP和端口，一般sentinel至少3个节点
