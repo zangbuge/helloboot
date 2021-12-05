07098f813423fce04381cd54975b47c37fac4f70
lihuiming <1469987440@qq.com>
2021-12-05 14:11:36 +0800

    调整redis配置

--- src/main/java/com/hugmount/helloboot/config/RedissonConfig.java
+++ src/main/java/com/hugmount/helloboot/config/RedissonConfig.java
@@ -3,11 +3,25 @@ package com.hugmount.helloboot.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedissonConfig {
+
+    @Value("${spring.redis.host}")
+    private String idAddr;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Value("${spring.redis.password}")
+    private String password;
+
+    @Value("${spring.redis.timeout}")
+    private int timeout;
+
 
     @Bean
     public RedissonClient redissonClient() {
@@ -17,9 +31,12 @@ public class RedissonConfig {
 //        .setMasterName("my-sentinel-name") // 设置sentinel.conf配置里的sentinel别名
 //         这里设置sentinel节点的服务IP和端口，一般sentinel至少3个节点
 //        .addSentinelAddress("redis://127.0.0.1:26379")
+
+        String addr = "redis://" + idAddr + ":" + port;
         config.useSingleServer()
-                .setPassword("123456")
-                .setAddress("redis://115.159.66.110:6379");
+                .setTimeout(timeout)
+                .setPassword(password)
+                .setAddress(addr);
         RedissonClient redisson = Redisson.create(config);
         return redisson;
     }
