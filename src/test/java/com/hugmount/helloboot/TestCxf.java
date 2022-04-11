09449dad5c5d9be5fe8f5b54e0326f88164e57bd
lihuiming <1469987440@qq.com>
2022-04-11 20:00:56 +0800

    JAXBElement 变成 String 类型

--- src/test/java/com/hugmount/helloboot/TestCxf.java
+++ src/test/java/com/hugmount/helloboot/TestCxf.java
@@ -22,7 +22,7 @@ public class TestCxf {
         // 解压apache-cxf-2.6.2.zip 进入bin目录
         // 执行wsdl2java命令
         // wsdl2java -p com -d src -all wsdlUrl
-        // wsdl2java -d src -p com.hugmount.helloboot.cxf.cg -encoding utf-8 -client wsdlUrl
+        // wsdl2java -d src -p com.hugmount.helloboot.cxf.cg -encoding utf-8 -b removeJAXBElement.xml -client wsdlUrl
         //-p  指定其wsdl的命名空间，也就是要生成代码的包名
         //-d  指定要产生代码所在目录
         //-client 生成客户端测试web service的代码
@@ -30,6 +30,7 @@ public class TestCxf {
         //-impl 生成web service的实现代码
         //-ant  生成build.xml文件
         //-all 生成所有开始端点代码
+        //-b removeJAXBElement.xml  生成JAXBElement 变成 String 类型
         //https协议要把[?wsdl] 换成[?singlewsdl]
         HelloService userService = (HelloService) jaxWsProxyFactoryBean.create();
         String res = userService.sayHello("lhm");
