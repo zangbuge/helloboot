246527da5c2aeacabe5da257e2f3d623d6838bdf
lihuiming <1469987440@qq.com>
2022-04-16 09:23:27 +0800

    httpclient

--- src/main/java/com/hugmount/helloboot/util/HttpClientUtil.java
+++ src/main/java/com/hugmount/helloboot/util/HttpClientUtil.java
@@ -4,12 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections.MapUtils;
 import org.apache.http.Consts;
 import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.entity.mime.HttpMultipartMode;
@@ -17,13 +14,11 @@ import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.protocol.HTTP;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.util.EntityUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.net.URI;
-import java.nio.charset.Charset;
 import java.util.Map;
 
 /**
@@ -34,152 +29,109 @@ import java.util.Map;
 @Slf4j
 public class HttpClientUtil {
 
-    private static final String TYPE_GET = "get";
-    private static final String TYPE_POST = "post";
+    private static CloseableHttpClient httpClient;
 
-    public static String doPostJson(String url, String json, Map<String, Object> header) {
-        return doPostJson(url, json, header, null);
+    static {
+        httpClient = getClient();
     }
 
-    public static String doPostJson(String url, String json, Map<String, Object> header, CloseableHttpClient httpClient) {
-        if (null == httpClient) {
-            // 创建Httpclient对象
-            httpClient = HttpClients.createDefault();
-        }
-
-        CloseableHttpResponse response = null;
-        String resultStr = null;
+    /**
+     * 发送post请求 json数据格式
+     *
+     * @param url
+     * @param json
+     * @param header
+     * @return
+     */
+    public static String doPostJson(String url, String json, Map<String, Object> header) {
         try {
             // 创建Http Post请求
             HttpPost httpPost = new HttpPost(url);
             // 创建请求内容
             StringEntity entity = new StringEntity(json, ContentType.APPLICATION_JSON);
             httpPost.setEntity(entity);
+            RequestConfig config = createConfig();
+            httpPost.setConfig(config);
             if (MapUtils.isNotEmpty(header)) {
                 for (Map.Entry<String, Object> map : header.entrySet()) {
                     httpPost.addHeader(map.getKey(), map.getValue().toString());
                 }
             }
             // 执行http请求
-            response = httpClient.execute(httpPost);
-            resultStr = EntityUtils.toString(response.getEntity(), "utf-8");
+            CloseableHttpResponse response = httpClient.execute(httpPost);
+            // EntityUtils.toString()会关闭流且释放连接
+            String result = EntityUtils.toString(response.getEntity(), Consts.UTF_8);
+            return result;
         } catch (Exception e) {
-            e.printStackTrace();
+            throw new RuntimeException("httpClient请求异常", e);
         }
-
-        return resultStr;
     }
 
-    public static String doPost(String url, Map<String, Object> param, CloseableHttpClient httpClient) {
-        return  sendRequestKeepAlive(httpClient, url, param, TYPE_POST, null);
-    }
-
-    public static String doGet(String url, Map<String, Object> param, CloseableHttpClient httpClient) {
-        return  sendRequestKeepAlive(httpClient, url, param, TYPE_GET, null);
-    }
-
-
-    public static CloseableHttpClient getClient() {
-        CloseableHttpClient httpClient = HttpClients.custom().build();
-        return httpClient;
-    }
-
-
-
-    public static String uploadFile(String url, File file, String name, Map<String, Object> param
-            , CloseableHttpClient httpClient) {
-
-        CloseableHttpResponse response;
+    /**
+     * 发送post请求 表单数据格式,可传文件
+     *
+     * @param url
+     * @param file
+     * @param name
+     * @param fromData
+     * @param header
+     * @return
+     */
+    public static String doPostForm(String url, File file, String name, Map<String, Object> fromData, Map<String, Object> header) {
         try {
-            if (null == httpClient) {
-                httpClient = getClient();
-            }
-
-            HttpPost httpPost = new HttpPost(url);
             // 相当于<input type="file" name="file"/>
             MultipartEntityBuilder builder = MultipartEntityBuilder.create();
-            builder.setCharset(Charset.forName("utf-8"));
-            //以浏览器兼容模式运行，防止文件名乱码。必须
+            builder.setCharset(Consts.UTF_8);
+            // 以浏览器兼容模式运行，防止文件名乱码。必须
             builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
             builder.addBinaryBody(name, new FileInputStream(file), ContentType.DEFAULT_BINARY, file.getName());
-            for (Map.Entry<String, Object> entry : param.entrySet()) {
+            for (Map.Entry<String, Object> entry : fromData.entrySet()) {
                 String key = entry.getKey();
                 String obj = entry.getValue().toString();
-
                 // 相当于<input type="text" name="userName" value=userName>
                 StringBody value = new StringBody(obj, ContentType.create("text/plain", Consts.UTF_8));
                 builder.addPart(key, value);
             }
-            HttpEntity reqEntity = builder.build();
-            httpPost.setEntity(reqEntity);
+            HttpEntity entity = builder.build();
+            HttpPost httpPost = new HttpPost(url);
+            if (MapUtils.isNotEmpty(header)) {
+                for (Map.Entry<String, Object> map : header.entrySet()) {
+                    httpPost.addHeader(map.getKey(), map.getValue().toString());
+                }
+            }
+            httpPost.setEntity(entity);
             // 发起请求 并返回请求的响应
-            response = httpClient.execute(httpPost);
+            CloseableHttpResponse response = httpClient.execute(httpPost);
             // 获取响应对象
-            HttpEntity resEntity = response.getEntity();
-            if (resEntity != null) {
-                return EntityUtils.toString(resEntity, Consts.UTF_8);
-            }
-        }catch (Exception e) {
-            log.error("上传文件失败", e);
-        }
-        return null;
-    }
-
-
-    public static String sendRequestKeepAlive(CloseableHttpClient httpClient, String url, Map<String, Object> param
-            ,String type ,Map<String, Object> header) {
-
-        try {
-            if (httpClient == null) {
-                httpClient = getClient();
-            }
-
-            // 创建uri
-            URIBuilder builder = new URIBuilder(url);
-            if (param != null) {
-                for (String key : param.keySet()) {
-                    builder.addParameter(key, param.get(key).toString());
-                }
-            }
-
-            HttpResponse httpResponse;
-            // 创建http GET 或 Post 请求
-            URI uri = builder.build();
-            if (TYPE_GET.equals(type)) {
-                HttpGet httpGet = new HttpGet(uri);
-                httpResponse = httpClient.execute(httpGet);
-            }
-            else {
-                HttpPost httpPost = new HttpPost(uri);
-                RequestConfig config = createConfig();
-                httpPost.setConfig(config);
-                // 设置请求头信息
-                if (MapUtils.isNotEmpty(header)) {
-                    for (Map.Entry<String, Object> map : header.entrySet()) {
-                        httpPost.addHeader(map.getKey(), map.getValue().toString());
-                    }
-                }
-                // 执行请求
-                httpResponse = httpClient.execute(httpPost);
-            }
-
-            HttpEntity entity = httpResponse.getEntity();
-            String res = EntityUtils.toString(entity, "UTF-8");
+            String res = EntityUtils.toString(response.getEntity(), Consts.UTF_8);
             return res;
         } catch (Exception e) {
-            log.error("请求失败,url: {}", url, e);
+            throw new RuntimeException("httpClient上传文件异常", e);
         }
-        return null;
+    }
+
+    public static CloseableHttpClient getClient() {
+        PoolingHttpClientConnectionManager manager = new PoolingHttpClientConnectionManager();
+        //检测有效链接的间隔
+        manager.setValidateAfterInactivity(9000);
+        //设定链接池最大数量
+        manager.setMaxTotal(500);
+        //设定默认单个路由的最大链接数（因为本处只使用一个路由地址因此设定为链接池大小）
+        manager.setDefaultMaxPerRoute(1000);
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .disableAutomaticRetries()
+                .setConnectionManager(manager)
+                .build();
+        return httpClient;
     }
 
     private static RequestConfig createConfig() {
-        RequestConfig.Builder builder = RequestConfig.custom();
-        builder.setConnectTimeout(1000 * 6)
+        RequestConfig config = RequestConfig.custom()
+                .setConnectTimeout(1000 * 6)
                 .setConnectionRequestTimeout(1000 * 2)
-                .setRedirectsEnabled(false) // 配置禁用30x 跳转
                 .setSocketTimeout(1000 * 30)
                 .build();
-        return builder.build();
+        return config;
     }
 
 }
--- src/test/java/com/hugmount/helloboot/TestHttpUtil.java
+++ src/test/java/com/hugmount/helloboot/TestHttpUtil.java
@@ -2,8 +2,8 @@ package com.hugmount.helloboot;
 
 import com.alibaba.fastjson.JSON;
 import com.hugmount.helloboot.util.HttpClientUtil;
-import org.apache.http.impl.client.CloseableHttpClient;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,34 +18,19 @@ public class TestHttpUtil {
     public static void main(String[] args) {
 
         String testJson = "http://127.0.0.1:8001/testJson";
-        String testPost = "http://127.0.0.1:8001/testPost";
         Map<String, Object> header = new HashMap<>();
-        header.put("token","1234567890");
+        header.put("token", "1234567890");
         Map<String, Object> map = new HashMap<>();
-        map.put("userId","123");
-        map.put("nickname","李会明");
-        CloseableHttpClient client = HttpClientUtil.getClient();
-        String json = HttpClientUtil.doPostJson(testJson, JSON.toJSONString(map), null);
+        map.put("userId", "123");
+        map.put("nickname", "李会明");
+        String json = HttpClientUtil.doPostJson(testJson, JSON.toJSONString(map), header);
         System.out.println("testJson返回: " + json);
 
-        String post = HttpClientUtil.doPost(testPost, map,null);
-        System.out.println("testPost返回" + post);
+        String testFile = "http://127.0.0.1:8001/testFile";
+        File file = new File("d:/wx.png");
+        String file1 = HttpClientUtil.doPostForm(testFile, file, "file", map, header);
+        System.out.println("testFile返回: " + file1);
 
-        String s = HttpClientUtil.sendRequestKeepAlive(null, testPost, map, "post", header);
-        System.out.println(s);
-
-        String ss = HttpClientUtil.doPostJson(testJson, JSON.toJSONString(map), header);
-        System.out.println(ss);
-
-        String url = "http://localhost:8086/helloboot/product/getProductList";
-        header.put("accept", "*/*");
-        header.put("connection", "Keep-Alive");
-        header.put("Content-Type", "application/json");
-        header.put("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
-        // 发送POST请求必须设置如下两行
-//        String s = HttpClientUtil.sendPostKeepAlive(client, url ,map ,null);
-//        System.out.println(s);
-//        System.out.println("success");
     }
 
 }
