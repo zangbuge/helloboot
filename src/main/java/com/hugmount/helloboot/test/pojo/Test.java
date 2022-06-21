7a559cda5a8d51bfc96146068177cdf20f4313ae
lhm <1469987440@qq.com>
2022-06-21 20:34:06 +0800

    @JsonSerialize

--- src/main/java/com/hugmount/helloboot/test/pojo/Test.java
+++ src/main/java/com/hugmount/helloboot/test/pojo/Test.java
@@ -1,11 +1,19 @@
 package com.hugmount.helloboot.test.pojo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
-/** 测试表
+/**
+ * 测试表
+ *
  * @Author: Li Huiming
  * @Date: 2019/3/11
  */
@@ -16,6 +24,13 @@ public class Test implements Serializable {
 
     private Long id;
 
+    /**
+     * 制定时间类型序列化处理类, 否则redis序列化时报错
+     */
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime creatTime;
 
 }
