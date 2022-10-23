d537d47b2501f71094814bf1ae819618e9838eb5
lhm <1469987440@qq.com>
2022-10-23 08:35:18 +0800

    仅格式化

--- src/main/java/com/hugmount/helloboot/core/Result.java
+++ src/main/java/com/hugmount/helloboot/core/Result.java
@@ -6,72 +6,73 @@ import lombok.Data;
 
 import java.io.Serializable;
 
-/** 服务器响应vo
- *  阿里规范：命名必须遵守驼峰形式，DO/DTO/VO 除外。 正确命名如： XmlService, TcpService
- *  1.数据对象：xxxDO，xxx即为数据表名；
- *  2.数据传输对象：xxxDTO，xxx为业务领域相关的名称；
- *  3.展示对象：xxxVO，xxx一般为网页的名称；
- *  4.POJO 是DO/DTO/BO/VO的统称，禁止命名成xxxPOJO。
+/**
+ * 服务器响应vo
+ * 阿里规范：命名必须遵守驼峰形式，DO/DTO/VO 除外。 正确命名如： XmlService, TcpService
+ * 1.数据对象：xxxDO，xxx即为数据表名；
+ * 2.数据传输对象：xxxDTO，xxx为业务领域相关的名称；
+ * 3.展示对象：xxxVO，xxx一般为网页的名称；
+ * 4.POJO 是DO/DTO/BO/VO的统称，禁止命名成xxxPOJO。
  *
  * @Author: Li Huiming
  * @Date: 2019/3/14
  */
 
-@ApiModel(value = "Result" ,discriminator = "服务器响应vo")
+@ApiModel(value = "Result", discriminator = "服务器响应vo")
 @Data
-public class Result<T> implements Serializable{
+public class Result<T> implements Serializable {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	//成功
-	private final static String SUCCESS = "0";
-	
-	//失败
-	private final static String ERROR = "1";
+    //成功
+    private final static String SUCCESS = "0";
 
-	//异常
-	private  final static String EXCEPTION = "-1";
+    //失败
+    private final static String ERROR = "1";
 
-	@ApiModelProperty(value = "代码")
-	private String code;
+    //异常
+    private final static String EXCEPTION = "-1";
 
-	@ApiModelProperty(value = "提示信息")
-	private String msg;
+    @ApiModelProperty(value = "代码")
+    private String code;
 
-	@ApiModelProperty(value = "数据")
-	private T data;
+    @ApiModelProperty(value = "提示信息")
+    private String msg;
 
-	public boolean isSuccess(){
-		return SUCCESS.equals(code);
-	}
-	
-	public static <T> Result<T> createBySuccess(String msg){
-		Result<T> result = new Result<>();
-		result.setCode(SUCCESS);
-		result.setMsg(msg);
-		return result;
-	}
-	
-	public static <T> Result<T> createBySuccess(String msg ,T data){
-		Result<T> result = new Result<>();
-		result.setCode(SUCCESS);
-		result.setMsg(msg);
-		result.setData(data);
-		return result;
-	}
-	
-	public static <T> Result<T> createByError(String msg){
-		Result<T> result = new Result<>();
-		result.setCode(ERROR);
-		result.setMsg(msg);
-		return result;
-	}
+    @ApiModelProperty(value = "数据")
+    private T data;
 
-	public static <T> Result<T> createByException(String msg){
-		Result<T> result = new Result<>();
-		result.setCode(EXCEPTION);
-		result.setMsg(msg);
-		return result;
-	}
+    public boolean isSuccess() {
+        return SUCCESS.equals(code);
+    }
+
+    public static <T> Result<T> createBySuccess(String msg) {
+        Result<T> result = new Result<>();
+        result.setCode(SUCCESS);
+        result.setMsg(msg);
+        return result;
+    }
+
+    public static <T> Result<T> createBySuccess(String msg, T data) {
+        Result<T> result = new Result<>();
+        result.setCode(SUCCESS);
+        result.setMsg(msg);
+        result.setData(data);
+        return result;
+    }
+
+    public static <T> Result<T> createByError(String msg) {
+        Result<T> result = new Result<>();
+        result.setCode(ERROR);
+        result.setMsg(msg);
+        return result;
+    }
+
+    public static <T> Result<T> createByException(String msg) {
+        Result<T> result = new Result<>();
+        result.setCode(EXCEPTION);
+        result.setMsg(msg);
+        return result;
+    }
 
 }
