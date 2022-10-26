cd0cfd007f111fcea4faece122bc708111528625
lhm <1469987440@qq.com>
2022-10-26 20:47:30 +0800

    格式化字符串

---
+++ src/test/java/com/hugmount/helloboot/TestStr.java
@@ -0,0 +1,18 @@
+package com.hugmount.helloboot;
+
+/**
+ * @author Li Huiming
+ * @date 2022/4/20
+ */
+public class TestStr {
+    public static void main(String[] args) {
+        int youNumber = 126;
+        // 0 代表前面补充0
+        // 4 代表长度为4
+        // d 代表参数为正数型
+        String str = String.format("%04d", youNumber);
+        System.out.println(str); // 0001
+
+
+    }
+}
