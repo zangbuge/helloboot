8ebd1bbc9726a4cdfec45de36186ba347cef7fe0
lhm <1469987440@qq.com>
2023-02-11 18:03:46 +0800

    Optional

--- src/test/java/com/hugmount/helloboot/TestSupplier.java
+++ src/test/java/com/hugmount/helloboot/TestSupplier.java
@@ -2,6 +2,7 @@ package com.hugmount.helloboot;
 
 import cn.hutool.core.lang.func.Consumer3;
 import cn.hutool.core.lang.func.Supplier2;
+import com.hugmount.helloboot.test.pojo.User;
 import com.hugmount.helloboot.util.POIUtil;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
@@ -42,6 +44,14 @@ public class TestSupplier {
             String s = msg + "lhm";
             System.out.println(s);
         });
+
+        User user = null;
+        String s = Optional.ofNullable(user).map(User::getUsername).orElse("1");
+        System.out.println("获取值 " + s);
+        // 为空直接阻断
+        Optional.ofNullable(user).map(User::getUsername).orElseThrow(RuntimeException::new);
+
+
     }
 
 
