6f0a8da498a3ae7641b0790c95ec5982334e227f
lhm <1469987440@qq.com>
2023-03-01 14:34:37 +0800

    mock bug

--- src/test/java/com/hugmount/helloboot/HellobootApplicationTests.java
+++ src/test/java/com/hugmount/helloboot/HellobootApplicationTests.java
@@ -1,24 +1,27 @@
 package com.hugmount.helloboot;
 
 import cn.hutool.json.JSONUtil;
+import com.hugmount.helloboot.test.mapper.TestMapper;
 import com.hugmount.helloboot.test.service.impl.TestServiceImpl;
-import com.hugmount.helloboot.thrift.server.UserInfo;
-import com.hugmount.helloboot.thrift.server.impl.HelloServiceImpl;
-import org.apache.thrift.TException;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
-// 注意这里是静态引入
-import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
+// 注意Mockito这里是静态引入
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -32,14 +35,23 @@ public class HellobootApplicationTests {
      * 被依赖标记
      */
     @Mock
-    private TestServiceImpl testService;
+    private TestMapper testMapper;
 
     /**
      * 目标测试类锚点
      */
     @Autowired
     @InjectMocks
-    private HelloServiceImpl helloService;
+    private TestServiceImpl testService;
+
+    /**
+     * junit测试类中任意测试方法执行前,都会执行此方法
+     */
+    @Before
+    public void init() {
+        // 初始化所需的资源
+        MockitoAnnotations.initMocks(this);
+    }
 
     @Test
     public void contextLoads() {
@@ -48,15 +60,11 @@ public class HellobootApplicationTests {
         System.out.println("测试完成");
         List<com.hugmount.helloboot.test.pojo.Test> list = new ArrayList<>();
         com.hugmount.helloboot.test.pojo.Test test = new com.hugmount.helloboot.test.pojo.Test();
-        test.setPhone("158");
+        test.setPhone("1589799");
         list.add(test);
-        when(testService.getTestList(any())).thenReturn(list);
-        try {
-            UserInfo user = helloService.getUser(1);
-            System.out.println("mock数据" + JSONUtil.toJsonStr(user));
-        } catch (TException e) {
-            e.printStackTrace();
-        }
+        when(testMapper.getTestList(any())).thenReturn(list);
+        List<com.hugmount.helloboot.test.pojo.Test> testList = testService.getTestList(test);
+        System.out.println("mock数据" + JSONUtil.toJsonStr(testList));
     }
 
 }
