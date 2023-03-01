b0729c3339fe9f3d099392c70bd769636e462c2c
lhm <1469987440@qq.com>
2023-03-01 17:22:02 +0800

    暂不启用xxl-job

--- src/main/java/com/hugmount/helloboot/xxl/TestXxlJobHandler.java
+++ src/main/java/com/hugmount/helloboot/xxl/TestXxlJobHandler.java
@@ -3,6 +3,7 @@ package com.hugmount.helloboot.xxl;
 import com.xxl.job.core.biz.model.ReturnT;
 import com.xxl.job.core.context.XxlJobHelper;
 import com.xxl.job.core.handler.annotation.XxlJob;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -11,7 +12,7 @@ import java.util.Date;
  * @author: lhm
  * @date: 2023/1/3
  */
-
+@Profile("test")
 @Component
 public class TestXxlJobHandler {
 
--- src/main/java/com/hugmount/helloboot/xxl/XxlJobConfig.java
+++ src/main/java/com/hugmount/helloboot/xxl/XxlJobConfig.java
@@ -5,12 +5,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 /**
  * @author: lhm
  * @date: 2023/1/3
  */
 
+@Profile("test")
 @Slf4j
 @Configuration
 public class XxlJobConfig {
