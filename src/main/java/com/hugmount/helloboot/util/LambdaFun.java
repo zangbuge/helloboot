4cfbb18279f37e2dcccf94632e927b2c8cbebce5
lhm <1469987440@qq.com>
2023-03-16 12:45:38 +0800

    LambdaFun

--- src/main/java/com/hugmount/helloboot/util/LambdaFun.java
+++ src/main/java/com/hugmount/helloboot/util/LambdaFun.java
@@ -1,6 +1,8 @@
 package com.hugmount.helloboot.util;
 
 import java.io.Serializable;
+import java.lang.invoke.SerializedLambda;
+import java.lang.reflect.Method;
 import java.util.function.Function;
 
 /**
@@ -8,4 +10,14 @@ import java.util.function.Function;
  * @date 2023/3/15
  */
 public interface LambdaFun<T, R> extends Function<T, R>, Serializable {
+    default SerializedLambda getSerializedLambda() {
+        try {
+            Method writeReplace = this.getClass().getDeclaredMethod("writeReplace");
+            writeReplace.setAccessible(true);
+            Object sl = writeReplace.invoke(this);
+            return (SerializedLambda) sl;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
