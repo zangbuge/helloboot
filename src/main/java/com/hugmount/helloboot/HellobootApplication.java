17e4fc8eb389add54d9b04a9bae5cfb0867b4639
lhm <1469987440@qq.com>
2024-04-29 17:23:27 +0800

    Database URL

--- src/main/java/com/hugmount/helloboot/HellobootApplication.java
+++ src/main/java/com/hugmount/helloboot/HellobootApplication.java
@@ -3,10 +3,18 @@ package com.hugmount.helloboot;
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import com.github.pagehelper.autoconfigure.PageHelperAutoConfiguration;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+import java.util.List;
+
 
 /**
  * @Author: Li Huiming
@@ -22,10 +30,20 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
 })
 @EnableDubbo
 @EnableApolloConfig
+@Slf4j
 public class HellobootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HellobootApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner printDataSourceInfo(List<DataSource> list) {
+        return (ApplicationArguments args) -> {
+            for (DataSource dataSource : list) {
+                log.info("Database URL: {}", dataSource.getConnection().getMetaData().getURL());
+            }
+        };
     }
 
 }
